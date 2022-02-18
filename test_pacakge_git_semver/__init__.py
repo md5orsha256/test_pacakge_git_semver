ceb928bd01d6e1bf00dc353e4020c2a2168449bd
Stepanov Dmitry <stpnv.dmtry@gmail.com>
2022-02-18 13:17:31 +0600

    Init package

---
+++ test_pacakge_git_semver/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.1.0'
+
+print(f"Test package. Version: {__version__}")
---
+++ tests/test_test_pacakge_git_semver.py
@@ -0,0 +1,5 @@
+from test_pacakge_git_semver import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
